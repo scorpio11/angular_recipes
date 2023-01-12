a2953fd43d8ea77bc00d49df22b17955027ed5af
Eren <erenerciyes@gmail.com>
2023-01-12 07:51:07 +0300

    continue

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,28 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { Recipe } from "./recipe.model";
+
+@Injectable({providedIn:'root'})
+export class RecipeService{
+    selectedRecipe = new EventEmitter<Recipe>();
+    private recipes: Recipe[] = [
+        new Recipe('Tasty Schnitzel', 
+        'This is simply a test', 
+        'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+        [
+            new Ingredient('Meat', 1),
+            new Ingredient('French Fries', 20)
+        ]),
+        new Recipe('Big Fat Burger', 
+        'This is simply a test', 
+        'https://www.fatburgercanada.com/wp-content/uploads/2018/09/fb18_FatCheeseBeer.png',
+        [
+            new Ingredient('Buns', 2),
+            new Ingredient('Meat', 1)
+        ])
+      ];
+
+      getRecipes(){
+        return this.recipes.slice();
+      }
+}
