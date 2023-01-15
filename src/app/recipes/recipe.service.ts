f3bbb45c7562da72895c2166c8356627fa862e9c
Eren <erenerciyes@gmail.com>
2023-01-15 22:02:33 +0300

    Observables

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,10 @@
 import { EventEmitter, Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 @Injectable({providedIn:'root'})
 export class RecipeService{
-    selectedRecipe = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
         new Recipe('Tasty Schnitzel', 
         'This is simply a test', 
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,10 @@
 import { EventEmitter, Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 
 @Injectable({providedIn:'root'})
 export class ShoppingListService{
-    ingredientsChanged=new EventEmitter<Ingredient[]>();
+    ingredientsChanged=new Subject<Ingredient[]>();
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10),
@@ -15,6 +16,6 @@ export class ShoppingListService{
 
     addIngredient(ingredient:Ingredient){
         this.ingredients.push(ingredient);
-        this.ingredientsChanged.emit(this.ingredients);
+        this.ingredientsChanged.next(this.ingredients);
     }
 }
